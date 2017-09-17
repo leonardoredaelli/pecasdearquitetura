373f37e45b3ff0c411333688ed9faca386da28aa
Lucas Schenatto <lucasvschenatto@users.noreply.github.com>
2017-09-17 19:21:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 app.use('/', express.static('public'));
 var port = 3000;
-app.listen(port, function() {
-    console.log(`Server running on http://localhost:${port}`);
+app.set('port', (process.env.PORT || 3000));
+app.listen(app.get('port'), function() {
+    console.log(`Server running on http://localhost:${app.get('port')}`);
     return "this request didn't reach the public folder"
 });
